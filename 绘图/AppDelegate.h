cb2873a4a8d2c3bdc2d0831965f92458835447c4
hongsechengzi <lanou3g@lianbinjing.local>
2014-06-15 17:26:15 +0800

    Initial Commit

---
+++ 绘图/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  绘图
+//
+//  Created by lanou3g on 14-6-15.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 绘图/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  绘图
+//
+//  Created by lanou3g on 14-6-15.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
